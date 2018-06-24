274ddcc1b1dc8b6d4943faf815eab22b0ae214ea
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-24 18:57:00 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,14 @@
 class Owner
+  @@owners = []
   attr_accessor :pets
 
   def initialize
     @pets = {fishes:[], cats:[],dogs:[]}
+    @@owners << self
+  end
+
+  def self.owner_count
+    @@owners.count
   end
 
 end
