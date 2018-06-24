610477cdae80b1bdd1ebbe10b756a5410a310aed
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-24 19:43:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,10 @@
 class Owner
   @@owners = []
   attr_accessor :pets
+  attr_reader :species
 
   def initialize
-    @pets = {fishes:[], cats:[],dogs:[]}
+    @pets => {fishes:[], cats:[],dogs:[]}
     @@owners << self
   end
 
