1a881e899bff06e8089073f215ff262f1807c5a9
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-24 21:34:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,17 +27,17 @@ class Owner
 
   def buy_fish(name)
     fish = Fish.new(name)
-    @species[:fishes] << fish
+    @pets[:fishes] << fish
   end
 
   def buy_cat(name)
     cat = Cat.new(name)
-    @species[:cats] << cat
+    @pets[:cats] << cat
   end
 
   def buy_dog(name)
     dog = Dog.new(name)
-    @species[:dogs] << dog
+    @pets[:dogs] << dog
   end
 
 end
